709118e14369d2e13712bb10cfc3edf60568934e
kelefakeita-ship-it <kelefakeita@gmail.com>
2026-05-20 21:46:05 +0200

    Lab 2.4: compliant GCS bucket module with evidence

---
+++ modules/compliant-gcs-bucket/README.m
@@ -0,0 +1,6 @@
+This module enforces the following NIST 800-53 controls on a GCS bucket:
+- SC-12: Cryptographic key establishment via customer-managed KMS key
+- SC-13: Cryptographic protection at rest with 90-day key rotation
+- SC-28: Protection of information at rest via CMEK AES-256
+- AU-11: Audit record retention via configurable retention policy
+- CM-6: Configuration settings via required compliance labels
